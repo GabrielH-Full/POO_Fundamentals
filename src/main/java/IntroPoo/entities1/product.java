542a06f0d737a2a8dd7e3a220333e580ab0fa37a
GabrielH-Full <gabrielhb2020@gmail.com>
2025-07-23 18:50:37 -0300

    Creating entities product

--- src/main/java/IntroPoo/entities1/product.java
+++ src/main/java/IntroPoo/entities1/product.java
@@ -1,4 +1,19 @@
 package IntroPoo.entities1;
 
 public class product {
+    public String name;
+    public double price;
+    public int quantity;
+
+    public double TotalValueStock() {
+        return price * quantity;
+    }
+
+    public void AddProducts(int quantity) {
+            this.quantity += quantity;
+    }
+
+    public void RemoveProducts(int quantity) {
+        this.quantity -= quantity;
+    }
 }
