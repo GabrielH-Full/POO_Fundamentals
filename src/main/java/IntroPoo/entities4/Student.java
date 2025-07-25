66600a4da1856d3e6b13faeeccce68fd37358f78
GabrielH-Full <gabrielhb2020@gmail.com>
2025-07-25 15:03:04 -0300

    Booting

--- src/main/java/IntroPoo/entities4/Student.java
+++ src/main/java/IntroPoo/entities4/Student.java
@@ -1,8 +1,22 @@
 package IntroPoo.entities4;
 
-public class boletim {
+public class Student {
     public String name;
-    public double nota1;
-    public double nota2;
-    public double nota3;
+    public double note1;
+    public double note2;
+    public double note3;
+
+
+    public double sun() {
+        return note1 + note2 + note3;
+    }
+
+    public double missingPoints() {
+            if (sun() < 60.0){
+                return 60.0 - sun();
+            }
+            else {
+                return 0.0;
+            }
+    }
 }
