6e014bf79b53402f8c3a67b3ce0df7d54d82b4ca
GabrielH-Full <gabrielhb2020@gmail.com>
2025-08-02 14:10:16 -0300

    booting

--- src/main/java/IntroPoo/Aplication8/program.java
+++ src/main/java/IntroPoo/Aplication8/program.java
@@ -1,4 +1,39 @@
 package IntroPoo.Aplication8;
 
+import IntroPoo.entities8.cadastro;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        cadastro[] cdt = new cadastro[10];
+
+        System.out.print("How many rooms will be rented?");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <=n; i++){
+                System.out.print("Rent #" + i + ":");
+                System.out.print("\nName: ");
+                String name = sc.next();
+                System.out.print("Email: ");
+                String email = sc.next();
+                System.out.print("Room: ");
+                int room = sc.nextInt();
+                cdt[room] = new cadastro(name, email);
+            }
+
+        System.out.println();
+        for (int i=1; i < 10; i++){
+            if (cdt[i] != null){
+                System.out.println("Busy rooms:");
+                System.out.println(i + ": " + cdt[i]);
+            }
+        }
+
+        sc.close();
+    }
 }
